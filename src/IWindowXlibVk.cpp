14d4312d8ccaed9760cb7cedaa668fe8b65a3c41
Immanuel Charles <73501113+Immanuel-C@users.noreply.github.com>
2023-02-23 11:36:45 -0500

    Update IWindowXlibVk.cpp

--- src/IWindowXlibVk.cpp
+++ src/IWindowXlibVk.cpp
@@ -38,7 +38,7 @@ namespace IWindow {
             extensionNames.push_back(VK_KHR_SURFACE_EXTENSION_NAME);
         }
 
-        VkResult CreateSurface(VkInstance instance, VkSurfaceKHR& surface, Window& window) {
+        VkResult CreateSurface(VkInstance instance, const VkSurfaceKHR& surface, Window& window) {
             VkXlibSurfaceCreateInfoKHR surfaceInfo{};
             surfaceInfo.sType = VK_STRUCTURE_TYPE_XLIB_SURFACE_CREATE_INFO_KHR;
             surfaceInfo.dpy = window.GetX11Display();
