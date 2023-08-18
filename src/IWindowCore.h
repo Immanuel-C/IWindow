2f30d5338b7e6bcd400cdd2bf9c52bd6e3b6e526
Immanuel Charles <73501113+Immanuel-C@users.noreply.github.com>
2023-08-18 17:21:05 -0400

    Need to define IWINDOW_DLL to build dlll

--- src/IWindowCore.h
+++ src/IWindowCore.h
@@ -2,11 +2,13 @@
 
 
 #ifdef _WIN32
-
+#ifdef IWINDOW_DLL
 #ifdef IWINDOW_BUILD_DLL
 #define IWINDOW_API __declspec(dllexport) 
 #else
 #define IWINDOW_API __declspec(dllimport)
 #endif
-
+#else
+#define IWINDOW_API
+#endif
 #endif
